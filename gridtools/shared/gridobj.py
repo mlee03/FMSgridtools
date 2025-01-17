02cc1127c767bd71f821da7b161738c89674d464
Frank Malatino <frank.malatino@noaa.gov>
2025-01-17 12:01:57 -0500

    Added Agrid lon,lat returing method to GridObj

--- gridtools/shared/gridobj.py
+++ gridtools/shared/gridobj.py
@@ -17,4 +17,24 @@ class GridObj:
     def write_out_grid(self, filepath: str):
         self.grid.to_netcdf(filepath)
 
+    def get_agrid_lonlat(self)-> tuple[npt.NDArray, npt.NDArray]:
+        D2R = np.pi/180
+        a_lon = None
+        a_lat = None
+        if self.grid.x.values is not None and self.grid.y.values is not None:
+            x_flat = self.grid.x.values.flatten()
+            y_flat = self.grid.y.values.flatten()
+            nx = self.grid.sizes['nx']//2
+            ny = self.grid.sizes['ny']//2
+
+            a_lon = np.zeros(shape=nx)
+            a_lat = np.zeros(shape=ny)
+
+            for i in range(nx):
+                a_lon[i] = x_flat[2*nx+1+2*i+1]*D2R
+            for j in range(ny):
+                a_lat[i] = y_flat[(2*j+1)*(2*nx+1)+1]*D2R
+
+        return a_lon, a_lat
+
 #TODO: I/O method for passing to the host
