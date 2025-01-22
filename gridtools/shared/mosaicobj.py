9162dcf2e692329931fb3b7627ca9c98427c7ffa
Halle Derry <Halle.Derry@ldt-4743543.gfdl.noaa.gov>
2025-01-22 13:43:59 -0500

    Resolved changes requested:  - use gridtools_utils for file checks  - use dataarray.sizes to get dimension names to lengths  - use gridtile names from mosaic file for griddict()

--- gridtools/shared/mosaicobj.py
+++ gridtools/shared/mosaicobj.py
@@ -1,12 +1,10 @@
 from typing import Optional, Dict
 from dataclasses import dataclass,field
-import sys
-import os
 import xarray as xr
 import numpy as np
 import numpy.typing as npt
-
 from gridtools_lib import GridObj
+from gridtools.shared.gridtools_utils import check_file_is_there
 
 @dataclass
 class MosaicObj:
@@ -25,67 +23,59 @@ class MosaicObj:
 
     def __post_init__(self):
         if self.mosaic_file is not None and self.gridfiles is None:
-            try:
-                self.dataset = xr.open_dataset(self.mosaic_file)
-            except FileNotFoundError:
-                sys.exit("No such file or directory: {}".format(self.mosaic_file))
+            check_file_is_there(self.mosaic_file)
+            self.dataset = xr.open_dataset(self.mosaic_file)
+
             self.gridfiles = self.get_gridfiles()
 
     def get_gridfiles(self):
         try:
-            files = list(self.dataset['gridfiles'][:].values.flatten())
-            return [f.decode('utf-8') for f in files]
+            return [ifile.decode('ascii') for ifile in self.dataset.gridfiles.values]
         except AttributeError:
                 print("Error: Mosaic file not provided as an attribute, unable to return gridfiles")
+
     def get_ntiles(self):
         try:
-            return  self.dataset['ntiles'].size
+            return  self.dataset.sizes['ntiles']
         except AttributeError:
             print("Error: Mosaic file not provided as an attribute, unable to return number of tiles")
 
-    def file_is_there(self, check_file:str): 
-        if os.path.isfile(check_file): 
-            print(f"File {check_file} exists.") 
-        else:
-            raise FileNotFoundError(f"File {check_file} does not exist.")
-
     def griddict(self):
-        i = 1
-        for file in self.gridfiles:
-            self.grid_dict[f'tile{i}'] = GridObj.from_file(file)
-            i+=1
+        gridtiles = [tile.decode('ascii') for tile in self.dataset.gridtiles.values]
+        for i in range(self.get_ntiles()):
+            self.grid_dict[gridtiles[i]] = GridObj.from_file(self.gridfiles[i])
 
     def write_out_mosaic(self):
-        
+
         mosaic = xr.DataArray(
-                [self.mosaic_name], 
-                attrs=dict(standard_name="grid_mosaic_spec", contact_regions="contacts", 
+                [self.mosaic_name],
+                attrs=dict(standard_name="grid_mosaic_spec", contact_regions="contacts",
                         children="gridtiles", grid_descriptor=""))
 
         gridlocation = xr.DataArray(
                     [self.gridlocation], attrs=dict(standard_name="grid_file_location"))
-        
+
         gridfiles = xr.DataArray(
                     data=self.gridfiles, dims=["ntiles"])
-        
+
         gridtiles = xr.DataArray(
                     data=self.gridtiles, dims=["ntiles"])
-        
+
         contacts = xr.DataArray(
-                   data=self.contacts, dims=["ncontact"], 
-                            attrs=dict(standard_name="grid_contact_spec", contact_type="boundary", 
+                   data=self.contacts, dims=["ncontact"],
+                            attrs=dict(standard_name="grid_contact_spec", contact_type="boundary",
                                 alignment="true", contact_index="contact_index", orientation="orient"))
-        
+
         contact_index = xr.DataArray(
-                        data=self.contact_index, dims=["ncontact"], 
+                        data=self.contact_index, dims=["ncontact"],
                             attrs=dict(standard_name="starting_ending_point_index_of_contact"))
 
         out = xr.Dataset(
-            data_vars={"mosaic": mosaic, 
-                        "gridlocation": gridlocation, 
-                        "gridfiles": gridfiles, 
-                        "gridtiles": gridtiles, 
-                        "contacts": contacts, 
+            data_vars={"mosaic": mosaic,
+                        "gridlocation": gridlocation,
+                        "gridfiles": gridfiles,
+                        "gridtiles": gridtiles,
+                        "contacts": contacts,
                         "contact_index": contact_index})
 
         out.to_netcdf(self.output_file)
