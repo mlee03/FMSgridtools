719c7a350825a908f188d67c4290dd396a19a0e5
hderry <Halle.Derry@ldt-4743543.gfdl.noaa.gov>
2025-02-28 15:26:31 -0500

    make regional mosaic using MosaicObj

--- FMSgridtools/make_mosaic/make_mosaic.py
+++ FMSgridtools/make_mosaic/make_mosaic.py
@@ -253,9 +253,17 @@ def make_regional_mosaic(
         yt = global_m.grid_dict[f'tile{tile}'].y
         yarr = yt[round(2*j_min - 2):round(2*j_min - 2 + 2*ny+1), round(2*i_min - 2):round(2*i_min - 2 + 2*nx+1)]
 
+        outfile = f"regional_grid.tile{tile}.nc"
+        mosaic = "regional_mosaic.nc"
+
         regional_grid = RegionalGridObj(tile, xarr,
                                         yarr)
-        regional_grid.write_out_regional_grid(f"regional_grid.tile{tile}.nc")
+        regional_grid.write_out_regional_grid(outfile)
+
+        regional_mosaic = MosaicObj(mosaic_name="regoinal_mosaic",
+                               gridfiles=np.array([outfile]),
+                               gridtiles=np.array([f"tile{tile}"]))
+        regional_mosaic.write_out_regional_mosaic(mosaic)
 
         print("\nCongratulations: You have successfully run make_regional_mosaic")
 
