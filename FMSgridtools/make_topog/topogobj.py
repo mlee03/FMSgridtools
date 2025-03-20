70ecffe250ee16d67c8bc542ddab513ed2cb95cd
Ryan Mulhall <35538242+rem1776@users.noreply.github.com>
2025-03-20 09:18:28 -0400

    add IO for input files and add generation routine for rectangular_basin topography type (#26)
    Co-authored-by: rem1776 <Ryan.Mulhall@noaa.gov>

--- FMSgridtools/make_topog/topogobj.py
+++ FMSgridtools/make_topog/topogobj.py
@@ -3,6 +3,7 @@ import xarray as xr
 import numpy as np
 import numpy.typing as npt
 import dataclasses
+import itertools
 
 # represents topography output file created by make_topog
 # contains parameters for topography generation that aren't tied to a specific topography type
@@ -12,52 +13,80 @@ import dataclasses
 class TopogObj():
     output_name: str = None
     ntiles: int = None
-    nx: int = None
-    ny: int = None
+    nx: dict = dataclasses.field(default_factory=dict) 
+    ny: dict = dataclasses.field(default_factory=dict)
     x_refine: int = None
     y_refine: int = None
     scale_factor: float = None
-    depth: npt.NDArray[np.float64] = None
+    depth_vars: dict = dataclasses.field(default_factory=dict)
+    depth_vals: dict = dataclasses.field(default_factory=dict)
     dataset: xr.Dataset = None
     __data_is_generated: bool = False
+    global_attrs: dict = dataclasses.field(default_factory=dict)
+    dims: dict = dataclasses.field(default_factory=dict)
 
-    # sets up dataset variables
+    # applies any scaling factors or refinements given 
     def __post_init__(self):
-        depth_vars = dict()
-        depth_attrs = dict()
-        depth_attrs["standard_name"] = "topographic depth at T-cell centers"
-        depth_attrs["units"] = "meters"
-        depth_coords = xr.Coordinates( {'ny': np.empty(self.ny), 'nx': np.empty(self.nx)} )
-        # TODO check dims of depth
+        self.depth_vars = dict()
+        self.depth_attrs = dict()
+        self.depth_attrs["standard_name"] = "topographic depth at T-cell centers"
+        self.depth_attrs["units"] = "meters"
+
+        # make sure nx/ny dicts are given
+        if self.nx is None or self.ny is None:
+            raise ValueError("No nx/ny dictionaries provided, cannot construct TopogObj")
+
+        # adjust nx/ny for refinements and scaling factor
+        if self.x_refine is not None:
+            print(f"updating nx for x refine value of {self.x_refine}")
+            self.nx.update((tname, val/self.x_refine) for tname, val in self.nx.items()) 
+        if self.y_refine is not None:
+            print(f"updating ny for y refine value of {self.y_refine}")
+            self.ny.update((tname, val/self.y_refine) for tname, val in self.ny.items()) 
+        if self.scale_factor is not None:
+            print(f"updating nx/ny for scale factor value of {self.scale_factor}")
+            self.nx.update((tname, val*self.scale_factor) for tname, val in self.nx.items()) 
+            self.ny.update((tname, val*self.scale_factor) for tname, val in self.ny.items()) 
+
+        # set up coordinates and dimensions based off tile count and nx/ny values
         # if single tile exclude tile number in variable name
-        if (self.ntiles == 1):
-            depth_vars["depth"]= xr.DataArray(
-                coords=depth_coords,
-                dims=["ny", "nx"],
-                attrs=depth_attrs,
-            )
+        if self.ntiles == 1:
+            self.dims = ['ny', 'nx']
         # loop through ntiles and add depth_tile<n> variable for each
         else:
+            self.dims = []
             for i in range(1,self.ntiles+1):
-                depthVarName = "depth_tile" + str(i)
-                depth_vars[depthVarName] = xr.DataArray(
-                    coords=depth_coords,
-                    dims=["ny", "nx"],
-                    attrs=depth_attrs,
-                )
-        # create dataset from vars
-        self.ds = xr.Dataset(
-            data_vars = depth_vars,
-        )
-        self.ds = self.ds.expand_dims({'ntiles': self.ntiles})
-        self.ds = self.ds.drop_vars(['ny', 'nx'])
-        # TODO add global attrs
+                self.dims.append("ny_tile"+str(i))
+                self.dims.append("nx_tile"+str(i))
 
-    # just writes out the file
+    # writes out the file
     def write_topog_file(self):
         if(not self.__data_is_generated):
             print("Warning: write routine called but depth data not yet generated")
-        self.ds.to_netcdf(self.output_name)
+
+        # create xarray DataArrays for each output variable
+        # single tile
+        if self.ntiles == 1:
+            self.depth_vars['depth'] = xr.DataArray(
+                data = self.depth_vals['depth_tile1'],
+                dims = self.dims,
+                attrs = self.depth_attrs)
+        # multi-tile 
+        else:
+            for i in range(1,self.ntiles+1):
+                self.depth_vars['depth_tile'+str(i)] = xr.DataArray(
+                    data = self.depth_vals['depth_tile'+str(i)], 
+                    dims = self.dims[(i-1)*2:(i-1)*2+2],
+                    attrs = self.depth_attrs)
+
+        # create dataset (this excludes ntiles, since it is not used in a variable)
+        self.dataset = xr.Dataset( data_vars=self.depth_vars )
+        
+        # add any global attributes
+        if self.global_attrs is not None:
+            self.dataset.attrs = self.global_attrs
+        # write to file
+        self.dataset.to_netcdf(self.output_name)
 
     def make_topog_realistic( self,
         topog_file: str = None,
@@ -84,7 +113,11 @@ class TopogObj():
         pass
 
     def make_rectangular_basin(self, bottom_depth: float = None):
-        pass
+        self.depth_vals = {}
+        for tileName in list(self.nx.keys()):
+            self.depth_vals[f"depth_{tileName}"] = np.full( (int(self.ny[tileName]), int(self.nx[tileName])), bottom_depth)
+        self.__data_is_generated = True
+
 
     def make_topog_gaussian(self,
         gauss_scale: float = None,
