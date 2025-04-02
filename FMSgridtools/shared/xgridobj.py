15e07c2df73b4bdd54d29659b9515be814a8ad4a
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-04-02 14:39:58 -0400

    remove optionals, remove spaces

--- FMSgridtools/shared/xgridobj.py
+++ FMSgridtools/shared/xgridobj.py
@@ -1,6 +1,5 @@
 import ctypes as ct
 from dataclasses import dataclass
-from typing import Optional
 import xarray as xr
 from FMSgridtools.shared.gridtools_utils import check_file_is_there
 from FMSgridtools.shared.gridobj import GridObj
@@ -8,20 +7,20 @@ from FMSgridtools.shared.gridobj import GridObj
 @dataclass
 class XGridObj() :
 
-    src_mosaic : Optional[str] = None
-    tgt_mosaic : Optional[str] = None
-    restart_remap_file : Optional[str] = None
-    out_remap_file     : Optional[str] = 'remap.nc'
-    src_grid : Optional[GridObj] = None 
-    tgt_grid : Optional[GridObj] = None 
-    debug    : Optional[bool] = False
-    order    : Optional[int] = 1
-    on_gpu   : Optional[bool] = False
+    src_mosaic: str = None
+    tgt_mosaic: str = None
+    restart_remap_file: str = None
+    out_remap_file: str = 'remap.nc'
+    src_grid: GridObj = None 
+    tgt_grid: GridObj = None 
+    debug: bool = False
+    order: int = 1
+    on_gpu: bool = False
 
-    dataset : Optional[xr.Dataset] = None 
+    dataset : xr.Dataset = None 
     __dataset_exists = False
     
-    def __post_init__(self) :
+    def __post_init__(self):
 
         if self.__check_dataset()            or \
            self.__check_restart_remap_file() or \
@@ -38,28 +37,28 @@ class XGridObj() :
     
         
         
-    def read_remap_file(self) :
+    def read_remap_file(self):
         self.dataset = xr.open_dataset(self.restart_remap_file)
         self.__dataset_exists = True
         
                 
-    def write_remap_file(self) :
+    def write_remap_file(self):
         self.dataset.to_netcdf(self.out_remap_file)
 
 
-    def create_xgrid(self) :
+    def create_xgrid(self):
         if not any( i == self.order for i in (1,2) ) : raise RuntimeError("conservative order must be 1 or 2")
         #create_xgrid
     
         
-    def __check_dataset(self) :
+    def __check_dataset(self):
         if self.dataset is not None :
             self.__dataset_exists = True
             return True
         else : return False
         
         
-    def __check_restart_remap_file(self) :
+    def __check_restart_remap_file(self):
         
         if self.restart_remap_file is not None :
             check_file_is_there(self.restart_remap_file)
@@ -68,9 +67,9 @@ class XGridObj() :
         else : return False
 
         
-    def __check_mosaic(self) :
+    def __check_mosaic(self):
         
-        if self.src_mosaic is not None and self.tgt_mosaic is not None :
+        if self.src_mosaic is not None and self.tgt_mosaic is not None:
             # file checks are done in mosaic
             # self.src_grid = MosaicObj(self.src_mosaic).get_grid(), MosaicObj(self.src_mosaic).grid ?
             # self.tgt_grid = MosaicObj(self.tgt_mosaic).get_grid()
@@ -78,6 +77,6 @@ class XGridObj() :
         else : return False
 
         
-    def __check_grids(self) :
+    def __check_grids(self):
         return self.src_grid is not None and self.tgt_grid is not None
         
