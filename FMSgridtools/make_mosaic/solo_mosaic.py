30f273f10b29f2e5a4090937ba4de5f98cb4f52b
hderry <halle.derry@noaa.gov>
2025-05-02 12:51:56 -0400

    match mosaic objects

--- FMSgridtools/make_mosaic/solo_mosaic.py
+++ FMSgridtools/make_mosaic/solo_mosaic.py
@@ -70,10 +70,10 @@ def make(num_tiles,
         print(f"NOTE: There are {ncontact} contacts\n")
 
         if ncontact > 0:
-            mosaic = MosaicObj(mosaic_name=mosaic_name,
+            grid = MosaicObj(mosaic_name=mosaic_name,
                                gridlocation=dir_name,
                                gridfiles=tilefiles,
                                gridtiles=gridtiles,
                                contacts=contacts,
                                contact_index=contact_index)
-            mosaic.write_out_mosaic(f'{mosaic_name}.nc')
+            grid.write_out_mosaic(f'{mosaic_name}.nc')
