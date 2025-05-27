e19146ff374e6816c054ca9905dadf9c9d8065da
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-05-27 12:03:47 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ class CustomInstall(install):
         with open("compile_log.txt", "w") as f:
             subprocess.run(
                 ["cmake", ".."],
-                cwd="./FREnctools_lib/cfrenctools/c_build",
+                cwd="./frenctools/cfrenctools/c_build",
                 stdout=f,
                 stderr=subprocess.STDOUT,
                 check=True,
             )
             subprocess.run(
                 ["make"],
-                cwd="./FREnctools_lib/cfrenctools/c_build",
+                cwd="./frenctools/cfrenctools/c_build",
                 stdout=f,
                 stderr=subprocess.STDOUT,
                 check=True,
@@ -52,7 +52,7 @@ setup(
     extras_require=extras_requires,
     name="fmsgridtools",
     license="",
-    packages=find_namespace_packages(include=["fmsgridtools", "FMSgridtools.*", "frencctools", "frenctools.pyfrenctools.*"]),
+    packages=find_namespace_packages(include=["fmsgridtools", "fmsgridtools.*", "frencctools", "frenctools.pyfrenctools.*"]),
     include_package_data=True,
     version="0.0.1",
     zip_safe=False,
