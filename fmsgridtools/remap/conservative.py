8c45c03fd1d34c135d2ff00d83247b8171bec5f5
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-08-12 10:03:37 -0400

    untracked files

---
+++ fmsgridtools/remap/conservative.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from fmsgridtools.shared.xgridobj import XGridObj
+
+def remap(src_mosaic: str,
+          input_dir: str = "./",
+          output_dir: str = None,
+          input_file: str = None,
+          output_file: str = None,          
+          tgt_mosaic: str = None,
+          tgt_nlon: int = None,
+          tgt_nlat: int =  None,
+          lon_bounds: list = None,
+          lat_bounds: list = None,
+          kbounds: list = None,
+          tbounds: list = None,
+          order: int = 1,
+          static_file: str = None,
+          check_conserve: bool = False):
+
+    #create an xgrid object
+    xgrid = XGridObj(input_dir, src_mosaic, tgt_mosaic)
+
+    #create xgrid
+    xgrid.create_xgrid()
+
+    
+        
