d5f6980bb414dc5f73415fd1322dfae566ad6e47
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-10-02 15:38:08 -0400

    mpi

--- fmsgridtools/remap/conservative.py
+++ fmsgridtools/remap/conservative.py
@@ -1,3 +1,4 @@
+from mpi4py import MPI
 import numpy as np
 import xarray as xr
 
@@ -24,15 +25,12 @@ def remap(xgrid: type[XGridObj] = None,
           static_file: str = None,
           check_conserve: bool = False) -> XGridObj:
 
-  pyfms.fms.init()
-  pyfms.horiz_interp.init(ninterp=6)
-
   mydir = "/home/Mikyung.Lee/FRE-NCTools/test-benchmark/tests_fregrid/Testa-conserve1-output/"
   data_dir = "/home/Mikyung.Lee/FRE-NCTools/DONOTDELETEME_DATA/TESTS/TESTS_INPUT/Testa-input/"
   src_mosaic = "C96_mosaic.nc"
   tgt_mosaic = "lonlat_288x180_mosaic.nc"
   src_data = "00010101.atmos_month_aer"
-
+  
   #get input grid
   src_mosaic = MosaicObj(input_dir=mydir, mosaic_file=src_mosaic).read()
   src_grid_dict = src_mosaic.get_grid(toradians=True, agrid=True)
@@ -41,12 +39,27 @@ def remap(xgrid: type[XGridObj] = None,
   tgt_mosaic = MosaicObj(input_dir=mydir, mosaic_file=tgt_mosaic).read()
   tgt_grid_dict = tgt_mosaic.get_grid(toradians=True, agrid=True)
 
-  #src_tiles
-  src_tiles = []
+  #initialize fms
+  comm = MPI.COMM_WORLD
+  pyfms.fms.init(ndomain=len(tgt_grid_dict), localcomm=comm.py2f())
+  pyfms.horiz_interp.init(ninterp=6)
+
+  #domain
+  nx, ny = tgt_grid_dict['tile1'].nx, tgt_grid_dict['tile1'].ny
+  global_indices = [0, nx, 0, ny]
+  layout = pyfms.mpp_domains.define_layout(global_indices, ndivs=pyfms.mpp.npes())
+  domain = pyfms.mpp_domains.define_domains(global_indices=global_indices, layout=layout)                                   
+
+  indices = comm.gather([domain.isc, domain.iec, domain.jsc, domain.jec], root=0)
   
-  for tgt_tile, tgt_grid in tgt_grid_dict.items():
-    field_out = None
-    interp_ids = {}
+  for tgt_tile in tgt_grid_dict:
+
+    #get tgt grid on domain
+    isc, iec, jsc, jec = domain.isc, domain.iec, domain.jsc, domain.jec
+    tgt_x = np.ascontiguousarray(tgt_grid_dict[tgt_tile].x[jsc:jec+1, isc:iec+1], dtype=np.float64)
+    tgt_y = np.ascontiguousarray(tgt_grid_dict[tgt_tile].y[jsc:jec+1, isc:iec+1], dtype=np.float64)
+
+    interp_ids, src_tiles = {}, []
     for src_tile, src_grid in src_grid_dict.items():
 
       src_tiles.append(src_tile)
@@ -55,12 +68,12 @@ def remap(xgrid: type[XGridObj] = None,
       
       interp_ids[src_tile] = pyfms.horiz_interp.get_weights(lon_in=src_grid.x,
                                                  lat_in=src_grid.y,
-                                                 lon_out=tgt_grid.x,
-                                                 lat_out=tgt_grid.y,
+                                                 lon_out=tgt_x,
+                                                 lat_out=tgt_y,
                                                  nlon_in=src_grid.nx,
                                                  nlat_in=src_grid.ny,
-                                                 nlon_out=tgt_grid.nx,
-                                                 nlat_out=tgt_grid.ny,
+                                                 nlon_out=(iec-isc),
+                                                 nlat_out=(jec-jsc),
                                                  convert_cf_order=False
       )
 
@@ -68,7 +81,7 @@ def remap(xgrid: type[XGridObj] = None,
     no_regrid = ["land_mask", "average_T1", "average_T2", "average_DT", "time_bnds", "bk", "pk", "land_mask"]
     if len(src_tiles) > 0: datafile_tile1 = src_data + ".tile1"
     if scalar_variables is None:      
-      with xr.open_dataset(data_dir+"/"+datafile_tile1+".nc") as dataset:
+      with xr.open_dataset(data_dir+"/"+datafile_tile1+".nc", decode_cf=False) as dataset:
         scalar_variables = [key for key in dataset.data_vars if key not in no_regrid]
 
     fields = {}
@@ -98,9 +111,34 @@ def remap(xgrid: type[XGridObj] = None,
         
           field.save(data=tgt_data, klevel=k, timepoint=itime)                
 
-      print(variable, field.tgt_field.sizes)
+      gathered_data = comm.gather(field.tgt_field.values, root=0)
+                
+      if pyfms.mpp.pe() == 0:
+        
+        if field.dims.has_z and field.dims.has_t:
+          global_array = np.zeros((field.dims.ntime, field.dims.nz, ny, nx))
+          for i in range(4):
+            isc, iec, jsc, jec = indices[i]
+            global_array[:,:, jsc:jec, isc:iec] += gathered_data[i]
+        elif field.dims.has_z:
+          global_array = np.zeros((field.dims.nz, ny, nx))
+          for i in range(4):
+            isc, iec, jsc, jec = indices[i]
+            global_array[:, jsc:jec, isc:iec] += gathered_data[i]
+        elif field.dims.has_t:
+          global_array = np.zeros((field.dims.ntime, ny, nx))
+          for i in range(4):
+            isc, iec, jsc, jec = indices[i]            
+            global_array[:, jsc:jec, isc:iec] += gathered_data[i]
+        else:
+          global_array = np.zeros((ny, nx))
+          for i in range(4):
+            isc, iec, jsc, jec = indices[i]
+            global_array[jsc:jec, isc:iec] += gathered_data[i]
+
+      field.tgt_field = xr.DataArray(global_array)
       fields[variable] = field.complete_tgt_field()
-      
+
     xr.Dataset(data_vars=fields).to_netcdf("test.nc")    
 
     
