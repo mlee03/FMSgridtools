ac48ec9b3362b4553a4967581c4890b2b2511c6a
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-11-21 14:27:41 -0500

    turn off tests that need to be updated

--- tests/shared/test_mosaicobj.py
+++ tests/shared/test_mosaicobj.py
@@ -9,8 +9,8 @@ import fmsgridtools
 ntiles = 6
 ncontacts = 12
 
-ds = {}
-ds["mosaic"] = xr.DataArray(
+answers = {}
+answers["mosaic"] = xr.DataArray(
     data="test_mosaic",
     attrs=dict(
         standard_name="grid_mosaic_spec",
@@ -19,16 +19,16 @@ ds["mosaic"] = xr.DataArray(
         grid_descriptor="",
     ),
 )
-ds["gridlocation"] = xr.DataArray(
+answers["gridlocation"] = xr.DataArray(
     data="./", attrs=dict(standard_name="grid_file_location")
 )
-ds["gridfiles"] = xr.DataArray(
+answers["gridfiles"] = xr.DataArray(
     data=[f"C96.tile{i}.nc" for i in range(1, ntiles + 1)], dims=["ntiles"]
 )
-ds["gridtiles"] = xr.DataArray(
+answers["gridtiles"] = xr.DataArray(
     data=[f"tile{i}" for i in range(1, ntiles + 1)], dims=["ntiles"]
 )
-ds["contacts"] = xr.DataArray(
+answers["contacts"] = xr.DataArray(
     data=[
         "C384_mosaic:tile1::C384_mosaic:tile2",
         "C384_mosaic:tile1::C384_mosaic:tile3",
@@ -52,7 +52,7 @@ ds["contacts"] = xr.DataArray(
     ),
     dims=["ncontacts"],
 )
-ds["contact_index"] = xr.DataArray(
+answers["contact_index"] = xr.DataArray(
     data=[
         "768:768,1:768::1:1,1:768",
         "1:768,768:768::1:1,768:1",
@@ -70,7 +70,7 @@ ds["contact_index"] = xr.DataArray(
     dims=["ncontacts"],
     attrs=dict(standard_name="starting_ending_point_index_of_contact"),
 )
-example_ds = xr.Dataset(data_vars=ds)
+answers_ds = xr.Dataset(data_vars=answers)
 
 
 @pytest.fixture(autouse=True)
@@ -96,26 +96,30 @@ def test_read_and_write(set_fms_files):
 
     mosaicfile = "test_mosaic.nc"
 
+    # write mosaic file
     mosaic = fmsgridtools.MosaicObj(mosaicfile=mosaicfile)
-    mosaic.mosaic = example_ds.mosaic.data
-    mosaic.gridlocation = example_ds.gridlocation.data
-    mosaic.gridfiles = example_ds.gridfiles.data
-    mosaic.gridtiles = example_ds.gridtiles.data
-    mosaic.contacts = example_ds.contacts.data
-    mosaic.contact_index = example_ds.contact_index.data
+    mosaic.mosaic = answers_ds.mosaic.data
+    mosaic.gridlocation = answers_ds.gridlocation.data
+    mosaic.gridfiles = answers_ds.gridfiles.data
+    mosaic.gridtiles = answers_ds.gridtiles.data
+    mosaic.contacts = answers_ds.contacts.data
+    mosaic.contact_index = answers_ds.contact_index.data
 
     mosaic.write()
 
+    # delete object in order to read
     del mosaic
 
+    # read mosaic file
     mosaic = fmsgridtools.MosaicObj(mosaicfile=mosaicfile).read()
 
-    assert mosaic.mosaic == example_ds.mosaic.data
-    assert mosaic.gridlocation == example_ds.gridlocation.data
-    assert mosaic.gridfiles == list(example_ds.gridfiles.data)
-    assert mosaic.gridtiles == list(example_ds.gridtiles.data)
-    assert mosaic.contacts == list(example_ds.contacts.data)
-    assert mosaic.contact_index == list(example_ds.contact_index.data)
+    # check answers
+    assert mosaic.mosaic == answers_ds.mosaic.data
+    assert mosaic.gridlocation == answers_ds.gridlocation.data
+    assert mosaic.gridfiles == list(answers_ds.gridfiles.data)
+    assert mosaic.gridtiles == list(answers_ds.gridtiles.data)
+    assert mosaic.contacts == list(answers_ds.contacts.data)
+    assert mosaic.contact_index == list(answers_ds.contact_index.data)
 
 
 def test_get_grid(set_fms_files):
@@ -123,23 +127,26 @@ def test_get_grid(set_fms_files):
     pyfms.fms.init()
 
     mosaicfile = "test_get_grid.nc"
-    example_ds.to_netcdf(mosaicfile)
 
-    mosaic = fmsgridtools.MosaicObj(mosaicfile=mosaicfile).read()
+    # write mosaic file for testing
+    answers_ds.to_netcdf(mosaicfile)
 
-    # write grid
+    # write grid for testing
     xy = np.arange(0, 96, dtype=np.float64)
     x, y = np.meshgrid(xy, xy)
     for gridfile in mosaic.gridfiles:
         fmsgridtools.GridObj(x=x, y=y).write(gridfile)
 
-    # test get grid
+    # read mosaic file, get grid
+    mosaic = fmsgridtools.MosaicObj(mosaicfile=mosaicfile).read()
     grids = mosaic.get_grid()
+
+    # test grids have been read in correctly
     for tile in mosaic.gridtiles:
         np.testing.assert_array_equal(grids[tile].x, x)
         np.testing.assert_array_equal(grids[tile].y, y)
 
-    # test get grid in radians
+    # test get grids have been converted to radians correctly
     grids_radians = mosaic.get_grid(radians=True)
     for tile in mosaic.gridtiles:
         np.testing.assert_array_equal(grids_radians[tile].x, np.radians(x))
