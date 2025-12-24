73f73da8cff6d8d6080e7f57794202a2e605f9e8
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-12-24 12:25:10 -0500

    start dataobj rewrite

--- fmsgridtools/remap/conservative.py
+++ fmsgridtools/remap/conservative.py
@@ -2,30 +2,41 @@ import numpy as np
 
 from fmsgridtools.shared.xgridobj import XGridObj
 
-def remap(src_mosaic: str,
-          input_dir: str = "./",
-          output_dir: str = None,
-          input_file: str = None,
-          output_file: str = None,
-          tgt_mosaic: str = None,
-          tgt_nlon: int = None,
-          tgt_nlat: int =  None,
-          lon_bounds: list = None,
-          lat_bounds: list = None,
-          kbounds: list = None,
-          tbounds: list = None,
-          order: int = 1,
-          static_file: str = None,
-          check_conserve: bool = False):
+import pyfms
 
-    #create an xgrid object
-    xgrid = XGridObj(input_dir, src_mosaic_file=src_mosaic, tgt_mosaic_file=tgt_mosaic)
 
-    #create xgrid
-    xgrid.create_xgrid()
+def remap(input_dir: str = "./",
+      src_mosaic: str = None,
+      tgt_mosaic: str = None,
+      input_file: str = None,
+      output_dir: str = "./",
+      output_file: str = None,
+      scalar_variables: list[str] = None,
+      lon_bounds: list = None,
+      lat_bounds: list = None,
+      kbounds: list = None,
+      tbounds: list = None,
+      order: int = 1,
+      check_conserve: bool = False,
+      gpu: bool = False):      
 
-    #write
-    xgrid.write()
+      xgrid = XGridObj(input_dir=input_dir, src_mosaic_file = src_mosaic, tgt_mosaicfile=tgt_mosaic)
+      tgt_tiles = list(xgrid.tgt.grid.keys())
+      nsrc_tiles = list(xgrid.src.mosaic.ntiles)
+
+      pyfms.fms.init(ndomain=len(tgt_tiles))
+
+      for tgt_tile in tgt_tiles:
+
+            pyfms.horiz_interp.init(nsrc_tiles)
+            
+            xgrid.domain = pyfms.mpp_domains.define_domains([0, xgrid.tgt.grid.nx-1, 0, xgrid.tgt.grid.ny-1])
+            xgrid.set_target_tile(tgt_tile)
+            xgrid.get_interp()
+
+            
+
+
 
 
 
