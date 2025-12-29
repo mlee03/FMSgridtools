d28813559bce44d838a1c34a276d370fa97c24c2
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-12-29 15:15:46 -0500

    save

--- fmsgridtools/remap/conservative.py
+++ fmsgridtools/remap/conservative.py
@@ -31,31 +31,41 @@ def remap(input_dir: str = "./",
 
             pyfms.horiz_interp.init(len(src_tiles))
 
-            #get xgrid
-            xgrid.domain = pyfms.mpp_domains.define_domains([0, xgrid.tgt.grid[tgt_tile].nx-1, 0, xgrid.tgt.grid[tgt_tile].ny-1])
+            nx_tgt, ny_tgt = xgrid.tgt.grid[tgt_tile].nx, xgrid.tgt.grid[tgt_tile].ny
+
+            #get xgrid, make sure mpi works cause i don't think it will
+            xgrid.domain = pyfms.mpp_domains.define_domains([0, nx_tgt-1, 0, ny_tgt-1])
             xgrid.set_target_tile(tgt_tile)
             xgrid.get_interp()
 
             if input_file is None: return
+            if output_file is None: output_file = input_file
+            if output_dir is None: output_dir = input_dir
 
-            fileobj = FileObj(datafile=input_file, input_dir=input_dir, tiles=src_tiles)
+            src_fileobj = SrcFileObj(datafile=input_file, input_dir=input_dir, tiles=src_tiles)
+            tgt_fileobj = TgtFileObj(datafile=output_file, output_dir=output_dir, tile=tgt_tile)
 
-            for var in fileobj.variables:
+            for var in src_fileobj.variables:
 
-                variable = VariableObj(var, fileobj)
+                variable = VariableObj(var, src_fileobj)
                 variable.get_attributes()
 
                 times = list(range(variable.dims.time.size)) if variable.dims.time.here else [None]
                 klevels = list(range(variable.dims.z.size)) if variable.dims.z.here else [None]
 
+                #automatically sets up time and vertical levels
+                #find a way to not send in nx and ny
+                variable.init_tgt_data(self, nx=nx_tgt, ny=ny_tgt)
+
                 for itime in times:
                     for klevel in klevels:
+                        data_slice = np.zeros(ny_tgt, nx_tgt, dtype=variable.dtype)
                         for src_tile in src_tiles:
                             input_data = variable.slice(tile=src_tile, timepoint=itime, klevel=klevel, prepare_data=True)
-                            data = pyfms.horiz_interp.interp(xgrid.interps[src_tile].interp_id, input_data, convert_cf_order=False)
-                            print(data)
+                            data_slice += pyfms.horiz_interp.interp(xgrid.interps[src_tile].interp_id, input_data, convert_cf_order=False)
 
+                            #gather if parallel, for now, no
+                        variable.set_tgt_data(data_slice, timepoint=itime, klevel=klevel)
 
-                exit()
-
+                tgt_fileobj.set_dataarray(var, data_dict=variable.tgt_dict)
 
--- fmsgridtools/remap/variableobj.py
+++ fmsgridtools/remap/variableobj.py
@@ -2,6 +2,7 @@ from itertools import pairwise
 from pathlib import Path
 from types import SimpleNamespace
 
+import numpy as np
 import xarray as xr
 
 class DimObj():
@@ -46,7 +47,7 @@ class DimsObj():
         return repr_str
 
 
-class FileObj():
+class SrcFileObj():
 
     skip_variables = [
         "geolon_c", "geolat_c", "geolon_u", "geolat_u", "geolon_v", "geolat_v",
@@ -62,104 +63,131 @@ class FileObj():
 
         self.input_dir = str(input_dir)
         self.tiles = tiles
-        self.datafiles = {tile: Path(input_dir)/Path(datafile + f".{tile}.nc") for tile in tiles}
+        self.src_datafiles = {tile: Path(input_dir)/Path(datafile + f".{tile}.nc") for tile in tiles}
         self.static_files = {}
         self.variables = variables
 
-        with xr.open_dataset(self.datafiles[self.tiles[0]], decode_cf=False) as dataset:
+        self.src_datasets = {
+            tile: xr.open_dataset(self.src_datafiles[tile], decode_cf=False) for tile in self.tiles
+        }
 
-            # soil_area: 00010101.land_static.nc cell_area: 00010101.land_static_sg.nc
-            associated_files = dataset.attrs.get("associated_files")
-            if associated_files is not None:
-                stringsplit = associated_files.replace(":", " ").split()
-                for i in range(0, len(stringsplit), 2):
-                    self.static_files[stringsplit[i]] = {
-                        tile: Path(input_dir)/stringsplit[i+1].replace(".nc", f".{tile}.nc") for tile in self.tiles
-                    }
+        dataset = self.src_datasets[tiles[0]]
+        # soil_area: 00010101.land_static.nc cell_area: 00010101.land_static_sg.nc
+        associated_files = dataset.attrs.get("associated_files")
+        if associated_files is not None:
+            stringsplit = associated_files.replace(":", " ").split()
+            for i in range(0, len(stringsplit), 2):
+                self.static_files[stringsplit[i]] = {
+                    tile: Path(input_dir)/stringsplit[i+1].replace(".nc", f".{tile}.nc") for tile in self.tiles
+                }
 
-            #get list of variables
-            if self.variables is None:
-                self.variables = []
-                for variable in dataset:
-                    if variable in self.skip_variables:
-                        print(f"skipping {variable}")
-                    else:
-                        self.variables.append(variable)
-
+        #get list of variables
+        if self.variables is None:
+            self.variables = []
+            for variable in dataset:
+                if variable in self.skip_variables:
+                    print(f"skipping {variable}")
+                else:
+                    self.variables.append(variable)
 
     def __repr__(self):
         repr_str = "\n"
         repr_str += f"input_dir = {self.input_dir}\n"
         repr_str += f"tiles = {self.tiles}\n"
-        repr_str += f"datafiles = {self.datafiles}\n"
+        repr_str += f"datafiles = {self.src_datafiles}\n"
         repr_str += f"static_files = {self.static_files}\n"
         return repr_str
+
+
+
+class TgtFileObj():
+
+    def __init__(self, datadict: dict = {}):
+        self.dataarrays = datadict
+        self.name = None
+        self.dims = None
+
+    def set_dataarray(self, variable: str = None, data_dict: dict = None, dataarray: xr.DataArray = None):
+
+        if data_dict is not None:
+            self.dataarrays["variable" ] = xr.DataArray.from_dict(data_dict)
+        elif dataarray is not None:
+            self.dataarrays["variable"] = dataarray
+        else:
+            raise RuntimeError("must provide something")
+
+    def set_coords(self, grid):
+        pass
+
 
 
 class VariableObj():
 
     time_coder = xr.coders.CFDatetimeCoder(use_cftime=True)
 
-    def __init__(self, variable: str, fileobj: FileObj = None):
+    def __init__(self, variable: str, fileobj: SrcFileObj = None):
 
         self.variable = variable
         self.fileobj = fileobj
         self.dims = DimsObj()
 
+        self.dtype = None,
         self.missing_value = None,
         self.fill_value = None,
         self.offset = None,
         self.scale_factor = None
 
         self.static_files: {} = None
-        self.data = None
+        self.src_data = None
+
+        self.tgt_dict = {
+            "data": npt.NDArray = None,
+            "dims": list = None,
+            "attrs": dict =  None
+        }
 
 
     def get_attributes(self):
 
         tile = self.fileobj.tiles[0]
-        infile = self.fileobj.datafiles[tile]
+        dataset = self.fileobj.datasets[tile]
 
-        if not infile.exists:
-            raise RuntimeError("file does not exist")
+        if self.variable not in dataset:
+            raise RuntimeError("variable not found")
 
-        with xr.open_dataset(infile, decode_cf=False) as dataset:
+        dataarray = dataset[self.variable]
 
-            if self.variable not in dataset:
-                raise RuntimeError("variable not found")
-            dataarray = dataset[self.variable]
+        self.dims.get(dataarray)
+        self.dtype=dataarray.dtype
 
-            self.dims.get(dataarray)
+        attributes = dataarray.attrs
+        self.missing_value = attributes.get("missing_value")
+        self.fill_value = attributes.get("_FillValue")
+        self.offset = attributes.get("add_offset")
+        self.scale_factor = attributes.get("scale_factor")
 
-            attributes = dataarray.attrs
-            self.missing_value = attributes.get("missing_value")
-            self.fill_value = attributes.get("_FillValue")
-            self.offset = attributes.get("add_offset")
-            self.scale_factor = attributes.get("scale_factor")
-
-            if "area" in str(attributes.get("cell_method")):
-                cell_measures = str(attributes.get("cell_measures"))
-                if "area:" in cell_measures:
-                    self.static_files = self.fileobj.static_files[cell_measures.split()[1]]
+        if "area" in str(attributes.get("cell_method")):
+            cell_measures = str(attributes.get("cell_measures"))
+            if "area:" in cell_measures:
+                self.static_files = self.fileobj.static_files[cell_measures.split()[1]]
 
 
     def slice(self, tile: str = "tile1", timepoint: int = None, klevel: int = None, prepare_data: bool = False):
 
 
-        with xr.open_dataset(self.fileobj.datafiles[tile], decode_cf=False) as dataset:
+        dataset = self.fileobj.datasets[tile]
 
-            slice_dict = {}
+        slice_dict = {}
+        if klevel is not None and self.dims.z.here:
+            slice_dict[self.dims.z.name] = klevel
+        if timepoint is not None  and self.dims.time.here:
+            slice_dict[self.dims.time.name] = timepoint
 
-            if klevel is not None and self.dims.z.here:
-                slice_dict[self.dims.z.name] = klevel
-            if timepoint is not None  and self.dims.time.here:
-                slice_dict[self.dims.time.name] = timepoint
-
-            self.data = dataset[self.variable].isel(slice_dict).values
+        self.src_data = dataset[self.variable].isel(slice_dict).values
 
         if prepare_data:
             self.prepare_data()
-        return self.data
+        return self.src_data
 
 
     def prepare_data(self):
@@ -167,14 +195,47 @@ class VariableObj():
         #missing value mask
         missing_value_mask = None
         if self.missing_value is not None:
-            missing_value_mask = self.data == self.missing_value
+            missing_value_mask = self.src_data == self.missing_value
 
-        if self.offset is not None: self.data += self.offset
-        if self.scale_factor is not None: self.data *= self.scale_factor
+        if self.offset is not None: self.src_data += self.offset
+        if self.scale_factor is not None: self.src_data *= self.scale_factor
 
         #zero out missing values so it doens't contribute to remapping
         if missing_value_mask is not None:
-            self.data = xr.where(missing_value_mask, 0.0, self.data)
+            self.src_data = xr.where(missing_value_mask, 0.0, self.src_data)
 
-        return self.data
+        return self.src_data
 
+
+    def init_tgt_dict(self, nx: int, ny: int, ntimes: int = None, nz: int = None):
+
+        dims = []
+        if self.dims.time.here:
+            if ntimes is None: ntimes = self.dims.time.size
+        dims.append(ntimes)
+
+        if self.dims.z.here:
+            if nz is None: nz = self.dims.z.size
+        dims.append(nx)
+
+        dims += [self.dims.y.size, self.dims.x.size]
+
+        self.tgt_dict["dims"] = dims
+        self.tgt_dict["attrs"]= self.src_datasets[self.fileobj.tiles[0]][self.variable].attrs
+        self.tgt_dict["data"] = np.zeros((ntimes, nz, ny, nx), dtype=self.dtype)
+
+        return self.tgt_dict
+
+
+    def set_tgt_data(self, data: npt.NDArray, timepoint: int = None, klevel: int = None):
+
+        if timepoint is None and klevel is None:
+            self.tgt_dict["data"] = data
+        elif timepoint is not None and klevel is not None:
+            self.tgt_dict["data"][timepoint, klevel, :, :] = data
+        elif timepoint is not None:
+            self.tgt_dict["data"][timepoint, :, :] = data
+        elif klevel is not None:
+            self.tgt_dict["data"][klevel, :, :] = data
+
+        return self.tgt_dict
